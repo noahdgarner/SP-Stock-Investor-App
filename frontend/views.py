4a8f05cd897a54318f4bcfae97153892c2364d7e
Anthonyhelou <anthonyhelou44@gmail.com>
2019-05-03 14:16:58 -0700

    report test printing

--- frontend/views.py
+++ frontend/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
+import os, random, string
 from Screener.User import User
 from Screener.Analyzer import Analyzer
 from Screener.report_generator import reportGenerator
 from ratelimit.decorators import ratelimit
-
+from django.template.loader import render_to_string
 # from django.http import HttpResponse
 # Create your views here.
 
@@ -96,4 +97,12 @@ def app(request):
         'graphpath': stat_path
     }
 
+    code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=5))
+
+    stat_path = 'frontend/static/output/' + code + '.html'
+
+    save_path = os.path.abspath(os.path.join(stat_path))
+    content = render_to_string('frontend/output.html', context)
+    with open(save_path, 'w') as static_file:
+        static_file.write(content)
     return render(request, "frontend/output.html", context)
