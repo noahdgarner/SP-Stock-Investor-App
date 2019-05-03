1c7ab3ba96e622040cde6b8e28033c53394a1bba
Noah Garner <noahguy16@gmail.com>
2019-05-03 15:28:36 -0700

    init commit local copy

--- Screener/UserRisk.py
+++
@@ -1,8 +0,0 @@
-class UserRisk:
-    def __init__(self):
-        self.riskScore = None
-        self.acceptableLosses = None
-
-    def calc_acceptableLosses(self):
-        lower_bound_1 = 0
-        upper_bound_1 = 0
--- frontend/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('app/', views.app, name='app'),
-    path('select/', views.select, name = 'select')
-]
